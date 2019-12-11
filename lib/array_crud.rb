598395010b16f8e48ba38b4110f7d6a8024727e9
lior Shine <shinelior@gmail.com>
2019-12-11 23:16:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,9 @@ end
 def add_element_to_end_of_array(array, element)
     array = ["wow", "I", "am", "really", "learning"]
     element = "arrays!"
-     array<< "a"
+     array<< "arrays!"
      
-     return
+   
 end
 
 def add_element_to_start_of_array(array, element)
