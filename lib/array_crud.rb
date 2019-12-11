3e80efd66d83e8199e81cab8839a5493c5e33bc7
lior Shine <shinelior@gmail.com>
2019-12-11 22:36:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
      
-    return array<<element
+    return array<< "element"
      
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  return array.unshift("")
 end
 
 def remove_element_from_end_of_array(array)
