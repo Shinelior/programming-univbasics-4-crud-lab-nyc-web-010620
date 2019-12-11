bc082ee93f4469652a6028c2644c5bde7b524336
lior Shine <shinelior@gmail.com>
2019-12-11 22:41:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- return an_array=["y","s","u","i"]
+ return an_array=["y","s","u","i","o"]
 end
 
 def add_element_to_end_of_array(array, element)
