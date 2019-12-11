aede4506bd0046cd942298033c32e725a6b0e876
lior Shine <shinelior@gmail.com>
2019-12-11 22:59:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,12 +27,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   return array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  
     array[index_number]
 end
 
